963829fb31ed87f6232040c5c1c435ecf11ebdc4
Soultan <soultntoure@gmail.com>
2024-07-05 04:18:58 +0300

    Learnt about exceptions and added them to improve the program functionality

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import * 
 from tkinter import messagebox
 import random
+import json
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -24,17 +27,30 @@ def save():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
-
+    new_data = {
+         website: {
+              "username": username,
+              "password": password,
+         }
+    }
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you fill all the fields")
     else:    
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {username}"
-                                    f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {username} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:    
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            
+            with open("data.json", "w") as data_file:    
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)   
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
